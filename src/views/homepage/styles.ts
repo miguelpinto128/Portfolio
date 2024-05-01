fdc29dfd1600ca7f27640f902acafd78a8234850
miguelpinto128 <miguelpinto128@gmail.com>
2024-05-01 13:02:18 +0100

    Fix scroll up button color

--- src/views/homepage/styles.ts
+++ src/views/homepage/styles.ts
@@ -8,7 +8,7 @@ export const ScrollTopWrapper = styled(Box)<{isActive?: boolean}>`
   position: fixed;
   bottom: 1rem;
   right: 1rem;
-  background: linear-gradient(90deg, rgba(246,27,16,1) 0%, rgba(239,9,99,1) 80%);
+  background: linear-gradient(180deg, rgba(31,81,255,1) 17%, rgba(7,17,140,1) 80%);
   width: auto;
   opacity: ${({ isActive }) => isActive ? 1 : 0};
   transition: opacity ${transitionsDelay} linear, transform ${transitionsDelay} linear;
